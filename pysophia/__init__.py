6567f17152f97dbffc27871440d7cb1e311b3d02
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-09-23 00:58:39 +0200

    Initial import

---
+++ pysophia/__init__.py
@@ -0,0 +1,103 @@
+
+__all__ = ['Sophia', 'SophiaError']
+
+from cffi import FFI
+from pysophia._sophia_ffi import ffi, lib
+
+
+# Load libc
+c_ffi = FFI()
+c_ffi.cdef("""
+void free(void *ptr);
+void * malloc(size_t size);
+""")
+c_lib = c_ffi.dlopen(None)
+
+
+class SophiaError(Exception):
+    pass
+
+
+class Sophia(object):
+    FL_RDWR   = lib.SPO_RDWR
+    FL_RDONLY = lib.SPO_RDONLY
+    FL_CREAT  = lib.SPO_CREAT
+
+    _env = None
+    _db = None
+    _closed = False
+
+    def __init__(self, path, flags):
+        if self._env is not None or self._db is not None:
+            raise RuntimeError('object was already initialized')
+        env = lib.sp_env()
+        if env == ffi.NULL:
+            raise MemoryError('could not allocate environment')
+        _flags = ffi.cast('int', flags)
+        _path = ffi.new('char[]', path)
+        r = lib.sp_ctl(env, lib.SPDIR, _flags, _path)
+        if r != 0:
+            error = lib.sp_error(env)
+            lib.sp_destroy(env)
+            raise SophiaError(ffi.string(error))
+        db = lib.sp_open(env)
+        if db == ffi.NULL:
+            error = lib.sp_error(env)
+            lib.sp_destroy(env)
+            raise SophiaError(ffi.string(error))
+        self._env = env
+        self._db = db
+
+    def close(self):
+        if self._closed:
+            return
+        self._closed = True
+        if self._db is not None:
+            lib.sp_destroy(self._db)
+        if self._env is not None:
+            lib.sp_destroy(self._env)
+
+    def set(self, key, value):
+        if self._closed:
+            raise RuntimeError('db is closed')
+        if not isinstance(key, basestring) or not isinstance(value, basestring):
+            raise TypeError('only unicode or byte string objects are supported')
+        _key = ffi.new('char[]', key)
+        _value = ffi.new('char[]', value)
+        r = lib.sp_set(self._db, _key, ffi.sizeof(_key), _value, ffi.sizeof(_value))
+        if r != 0:
+            error = lib.sp_error(self._db)
+            raise SophiaError(ffi.string(error))
+
+    def get(self, key):
+        if self._closed:
+            raise RuntimeError('db is closed')
+        if not isinstance(key, basestring):
+            raise TypeError('only unicode or byte string objects are supported')
+        _key = ffi.new('char[]', key)
+        _value = ffi.new('void**')
+        _value_size = ffi.new('size_t*')
+        r = lib.sp_get(self._db, _key, ffi.sizeof(_key), _value, _value_size)
+        if r == -1:
+            error = lib.sp_error(self._db)
+            raise SophiaError(ffi.string(error))
+        elif r == 0:
+            raise KeyError('%s not found' % key)
+        value = ffi.string(ffi.cast('char*', _value[0]), _value_size[0])
+        c_lib.free(_value[0])
+        return value
+
+    def delete(self, key):
+        if self._closed:
+            raise RuntimeError('db is closed')
+        if not isinstance(key, basestring):
+            raise TypeError('only unicode or byte string objects are supported')
+        _key = ffi.new('char[]', key)
+        r = lib.sp_delete(self._db, _key, ffi.sizeof(_key))
+        if r != 0:
+            error = lib.sp_error(self._db)
+            raise SophiaError(ffi.string(error))
+
+    def __del__(self):
+        self.close()
+
---
+++ pysophia/_sophia_ffi.py
@@ -0,0 +1,78 @@
+
+__all__ = ['ffi', 'lib']
+
+from cffi import FFI
+
+
+sophia_h = """
+typedef void *(*spallocf)(void *ptr, size_t size, void *arg);
+typedef int (*spcmpf)(char *a, size_t asz, char *b, size_t bsz, void *arg);
+
+typedef enum {
+	/* env related */
+	SPDIR,     /* uint32_t, char* */
+	SPALLOC,   /* spallocf, void* */
+	SPCMP,     /* spcmpf, void* */
+	SPPAGE,    /* uint32_t */
+	SPGC,      /* int */
+	SPGCF,     /* double */
+	SPGROW,    /* uint32_t, double */
+	SPMERGE,   /* int */
+	SPMERGEWM, /* uint32_t */
+	/* db related */
+	SPMERGEFORCE,
+	/* unrelated */
+	SPVERSION  /* uint32_t*, uint32_t* */
+} spopt;
+
+/* NOTE: this is an anonymous enum in sophia itself */
+typedef enum {
+	SPO_RDONLY = 1,
+	SPO_RDWR   = 2,
+	SPO_CREAT  = 4,
+	SPO_SYNC   = 8
+} spflags;
+
+typedef enum {
+	SPGT,
+	SPGTE,
+	SPLT,
+	SPLTE
+} sporder;
+
+typedef struct {
+	uint32_t epoch;
+	uint64_t psn;
+	uint32_t repn;
+	uint32_t repndb;
+	uint32_t repnxfer;
+	uint32_t catn;
+	uint32_t indexn;
+	uint32_t indexpages;
+} spstat;
+
+void *sp_env(void);
+void *sp_open(void *env);
+int sp_ctl(void*, spopt, ...);
+int sp_destroy(void *ptr);
+int sp_begin(void *db);
+int sp_commit(void *db);
+int sp_rollback(void *db);
+int sp_set(void *db, const void *k, size_t ksize, const void *v, size_t vsize);
+int sp_delete(void *db, const void *k, size_t ksize);
+int sp_get(void *db, const void *k, size_t ksize, void **v, size_t *vsize);
+void *sp_cursor(void *db, sporder, const void *k, size_t ksize);
+int sp_fetch(void *cur);
+const char *sp_key(void *cur);
+size_t sp_keysize(void *cur);
+const char *sp_value(void *cur);
+size_t sp_valuesize(void *cur);
+char *sp_error(void *ptr);
+void sp_stat(void *ptr, spstat*);
+"""
+
+ffi = FFI()
+ffi.cdef(sophia_h)
+
+lib = ffi.dlopen('sophia')
+
